695197ff35870a99b6050f8d229894fec9de2144
DADA YO <dayeon.yoon@gmail.com>
2023-01-28 21:13:46 +0900

    Movies Service 1

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,8 @@
+export class Movie {
+  // 임시 데이터베이스
+  // 서비스로 보내고 받을 클래스(인터페이스)를 export
+  id: number;
+  title: string;
+  year: number;
+  genres: string[];
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,32 +9,36 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
-  @Get('search')
-  search(@Query('year') searchingYear: string) {
-    return `We are searching for a movie made after ${searchingYear}`;
-  }
+  // @Get('search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are searching for a movie made after ${searchingYear}`;
+  // }
 
   @Get('/:id')
-  getOne(@Param('id') id: string) {
-    return `This will return one movie with the id: ${id}`;
+  getOne(@Param('id') id: string): Movie {
+    return this.moviesService.getOne(id);
   }
 
   @Post()
   create(@Body() movieData) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
   delete(@Param('id') id: string) {
-    return `This will delete a movie with the id: ${id}`;
+    return this.moviesService.deleteOne(id);
   }
 
   @Put() // 전체 업데이트 시에 사용
