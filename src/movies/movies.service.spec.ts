46b97efae012e53cfe347d397822bfcd102c10ae
DADA YO <dayeon.yoon@gmail.com>
2023-01-29 04:36:56 +0900

    Testing getAll and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -16,7 +17,33 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be 4', () => {
-    expect(2 + 4).toEqual(4);
+  describe('getAll()', () => {
+    // getAll이 array를 return하는지 테스트
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne()', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 Error', () => {
+      try {
+        service.getOne(999); // id에 999가 없으므로 당연히 동작 X => NotFoundException 발생
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`Movie with 999 not found`);
+      }
+    });
   });
 });
